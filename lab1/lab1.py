b5cf2b4fc4817b00e7c93b85a852048a222b6035
Вадим Турко <vadim.turko@gmail.com>
2021-09-13 16:21:42 +0300

    finished lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,14 +2,18 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 
 save_dir = 'result_images/'
-name = 'kodim01'
+name = 'kodim14'
 
 img = cv.imread('test_images/' + name + '.png')
 normalized = cv.normalize(img.astype('float'), None, 0.0, 1.0, cv.NORM_MINMAX)[...,::-1]
-gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)[...,::-1]
+gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 print('size: ', img.size, '\nheight: ',
       img.shape[0], '\nwidth: ', img.shape[1], '\nchannels: ', img.shape[2])
+
+      
+plt.imsave(save_dir + name + '_normalized.png', normalized)
+plt.imsave(save_dir + name + '_gray_image.png', gray_image, cmap='gray')
 
 plt.subplot(2, 3, 1)
 plt.imshow(img[...,::-1])
@@ -18,11 +22,9 @@ plt.colorbar()
 plt.subplot(2, 3, 2)
 plt.imshow(normalized)
 plt.colorbar()
-plt.imsave(save_dir + name + '_normalized.png', normalized)
 
 plt.subplot(2, 3, 3)
 plt.imshow(gray_image, cmap='gray', vmin=0, vmax=255)
 plt.colorbar()
-plt.imsave(save_dir + name + '_gray_image.png', gray_image, cmap='gray')
 
 plt.show()
