ac0093988de0746dbff00d2f43a9d3965693fb77
Вадим Турко <vadim.turko@gmail.com>
2021-09-13 14:03:34 +0300

    fix saving

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,29 +3,26 @@ import matplotlib.pyplot as plt
 
 save_dir = 'result_images/'
 name = 'kodim01'
+
 img = cv.imread('test_images/' + name + '.png')
+normalized = cv.normalize(img.astype('float'), None, 0.0, 1.0, cv.NORM_MINMAX)[...,::-1]
+gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)[...,::-1]
 
 print('size: ', img.size, '\nheight: ',
       img.shape[0], '\nwidth: ', img.shape[1], '\nchannels: ', img.shape[2])
 
-plt.matshow(img)
+plt.subplot(2, 3, 1)
+plt.imshow(img[...,::-1])
 plt.colorbar()
-plt.show()
-cv.imshow("original", img)
-cv.waitKey(0)
 
-normalized = cv.normalize(img.astype('float'), None, 0.0, 1.0, cv.NORM_MINMAX)
-cv.imwrite(save_dir + name + '_normalized.png', normalized)
-plt.matshow(normalized)
+plt.subplot(2, 3, 2)
+plt.imshow(normalized)
 plt.colorbar()
-plt.show()
-cv.imshow("normalized", normalized)
-cv.waitKey(0)
+plt.imsave(save_dir + name + '_normalized.png', normalized)
 
-gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imwrite(save_dir + name + '_gray_image.png', gray_image)
-plt.matshow(gray_image)
+plt.subplot(2, 3, 3)
+plt.imshow(gray_image, cmap='gray', vmin=0, vmax=255)
 plt.colorbar()
+plt.imsave(save_dir + name + '_gray_image.png', gray_image, cmap='gray')
+
 plt.show()
-cv.imshow("gray_image", gray_image)
-cv.waitKey(0)
