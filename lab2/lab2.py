6a65bb8765d18c9f945c69860c6c3a9be46f41c0
Вадим Турко <vadim.turko@gmail.com>
2021-09-14 09:35:02 +0300

    implemented image rotation

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,30 +1,74 @@
 import cv2 as cv
+import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+def rotate_image(image, angle):
+  height = image.shape[0]
+  width = image.shape[1]
+  output=np.zeros((height, width, image.shape[2]))
+  center_height = round(((image.shape[0]+1)/2)-1)
+  center_width = round(((image.shape[1]+1)/2)-1) 
+
+  angle=math.radians(angle)
+  cosine=math.cos(angle)
+  sine=math.sin(angle)
+
+  for i in range(image.shape[0]):
+    for j in range(image.shape[1]):
+        y=image.shape[0]-1-i-center_height                  
+        x=image.shape[1]-1-j-center_width                    
+
+        new_y=round(-x*sine+y*cosine)
+        new_x=round(x*cosine+y*sine)
+
+        new_y=center_height-new_y
+        new_x=center_width-new_x
+
+        if 0 <= new_x < width and 0 <= new_y < height and new_x>=0 and new_y>=0:
+            output[new_y,new_x,:]=image[i,j,:]
+            
+  normalized = cv.normalize(output.astype('float'), None, 0.0, 1.0, cv.NORM_MINMAX)
+  return normalized
+
+def bilinear_interpolate(im, x, y):
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    x0 = np.floor(x).astype(int)
+    x1 = x0 + 1
+    y0 = np.floor(y).astype(int)
+    y1 = y0 + 1
+
+    x0 = np.clip(x0, 0, im.shape[1]-1)
+    x1 = np.clip(x1, 0, im.shape[1]-1)
+    y0 = np.clip(y0, 0, im.shape[0]-1)
+    y1 = np.clip(y1, 0, im.shape[0]-1)
+
+    a = im[ y0, x0 ]
+    b = im[ y1, x0 ]
+    c = im[ y0, x1 ]
+    d = im[ y1, x1 ]
+
+    wa = (x1-x) * (y1-y)
+    wb = (x1-x) * (y-y0)
+    wc = (x-x0) * (y1-y)
+    wd = (x-x0) * (y-y0)
+
+    return wa*a + wb*b + wc*c + wd*d
 
 save_dir = 'result_images/'
 name = 'kodim14'
 
-img = cv.imread('test_images/' + name + '.png')
-normalized = cv.normalize(img.astype('float'), None, 0.0, 1.0, cv.NORM_MINMAX)[...,::-1]
-gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img = cv.imread('test_images/' + name + '.png')[...,::-1]
+print(img.shape)
+rotated = rotate_image(img, 45)
+scaled = bilinear_interpolate(img, 100, 100)
+print(scaled)
 
-print('size: ', img.size, '\nheight: ',
-      img.shape[0], '\nwidth: ', img.shape[1], '\nchannels: ', img.shape[2])
-
-      
-plt.imsave(save_dir + name + '_normalized.png', normalized)
-plt.imsave(save_dir + name + '_gray_image.png', gray_image, cmap='gray')
-
-plt.subplot(2, 3, 1)
-plt.imshow(img[...,::-1])
-plt.colorbar()
-
-plt.subplot(2, 3, 2)
-plt.imshow(normalized)
-plt.colorbar()
-
-plt.subplot(2, 3, 3)
-plt.imshow(gray_image, cmap='gray', vmin=0, vmax=255)
-plt.colorbar()
+plt.subplot(2, 2, 1)
+plt.imshow(img)
+plt.subplot(2, 2, 2)
+plt.imshow(rotated)
 
 plt.show()
