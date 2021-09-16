8953eb2f3e2e1eafe3288c648f56573a836558f4
Вадим Турко <vadim.turko@gmail.com>
2021-09-16 10:21:14 +0300

    fixes

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -5,20 +5,19 @@ import math
 
 
 def rotate_image(image, angle):
-    height = image.shape[0]
-    width = image.shape[1]
-    output = np.zeros((height, width, image.shape[2]))
-    center_height = round(((image.shape[0]+1)/2)-1)
-    center_width = round(((image.shape[1]+1)/2)-1)
+    h, w, c = image.shape
+    output = np.zeros((h, w, c))
+    center_height = round(h / 2)
+    center_width = round(w / 2)
 
     angle = math.radians(angle)
     cosine = math.cos(angle)
     sine = math.sin(angle)
 
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            y = image.shape[0]-1-i-center_height
-            x = image.shape[1]-1-j-center_width
+    for i in range(h):
+        for j in range(w):
+            y = h-1-i-center_height
+            x = w-1-j-center_width
 
             new_y = round(-x*sine+y*cosine)
             new_x = round(x*cosine+y*sine)
@@ -26,12 +25,10 @@ def rotate_image(image, angle):
             new_y = center_height-new_y
             new_x = center_width-new_x
 
-            if 0 <= new_x < width and 0 <= new_y < height and new_x >= 0 and new_y >= 0:
+            if 0 <= new_x < w and 0 <= new_y < h and new_x >= 0 and new_y >= 0:
                 output[new_y, new_x, :] = image[i, j, :]
 
-    normalized = cv.normalize(output.astype(
-        'float'), None, 0.0, 1.0, cv.NORM_MINMAX)
-    return normalized
+    return output
 
 
 def enlarge2(image):
@@ -48,23 +45,24 @@ def enlarge2(image):
     for j in range(w - 1):
         result[:, j * 2, :] = image_y[:, j, :]
         result[:, j * 2 + 1, :] = (image_y[:, j, :] + image_y[:, j + 1, :]) / 2
-    
-    return result
 
+    return result
 
 
 save_dir = 'result_images/'
 name = 'kodim14'
 
 img = cv.imread('test_images/' + name + '.png')[..., ::-1]
-rotated = rotate_image(img, 45)
-doubled = enlarge2(img)
+normalized = cv.normalize(img.astype(
+    'float'), None, 0.0, 1.0, cv.NORM_MINMAX)
+rotated = rotate_image(normalized, 45)
+doubled = enlarge2(normalized)
 
-plt.subplot(2, 2, 1)
+plt.subplot(1, 3, 1)
 plt.imshow(img)
-plt.subplot(2, 2, 2)
+plt.subplot(1, 3, 2)
 plt.imshow(rotated)
-plt.subplot(2, 2, 3)
-plt.imshow(doubled.astype(np.uint8))
+plt.subplot(1, 3, 3)
+plt.imshow(doubled)
 
 plt.show()
