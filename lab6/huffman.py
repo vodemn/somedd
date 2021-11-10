37069216df761375609d81e140c97ff8184ae993
Vadim <44135514+vodemn@users.noreply.github.com>
2021-11-10 22:16:16 +0300

    implemented huffmandict and huffmanenco

--- lab6/huffman.py
+++ lab6/huffman.py
@@ -1,13 +1,42 @@
 from cv2 import sort
 import numpy as np
 
+# https://www.w3resource.com/python-exercises/challenges/1/python-challenges-1-exercise-58.php
 
-def huffmandict(img):
-    values, counts = np.unique(img, return_counts=True)
 
-    inds = np.flip(counts.argsort())
-    values = values[inds]
-    counts = counts[inds]
-    print(values, counts)
+def huffmandict(counts_dict: dict):
+    from heapq import heappush, heappop, heapify
+    from collections import defaultdict
 
-huffmandict(['a', 'a', 'b', 'b', 'b', 'b', 'c'])
+    # mapping of letters to codes
+    code = defaultdict(list)
+
+    # Using a heap makes it easy to pull items with lowest frequency.
+    # Items in the heap are tuples containing a list of letters and the
+    # combined frequencies of the letters in the list.
+    heap = [(count, [value]) for value, count in counts_dict.items()]
+
+    # Reduce the heap to a single item by combining the two items
+    # with the lowest frequencies.
+    while len(heap) > 1:
+        freq0, letters0 = heappop(heap)
+        for ltr in letters0:
+            code[ltr].insert(0, '0')
+
+        freq1, letters1 = heappop(heap)
+        for ltr in letters1:
+            code[ltr].insert(0, '1')
+
+        heappush(heap, (freq0+freq1, letters0+letters1))
+
+    for k, v in code.items():
+        code[k] = ''.join(v)
+
+    return dict(sorted(code.items(), key=lambda item: int(item[1], 2)))
+
+
+def huffmanenco(img, huff_dict):
+    result = np.zeros(img.shape, dtype='int')
+    for coord, val in np.ndenumerate(img):
+        result[coord[0]][coord[1]] = int('0b' + huff_dict[val], 2)
+    return result
--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,33 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
-from math import log2
-from func import count_p, histogram, ref
-import sys
-# sys.path is a list of absolute path strings
-sys.path.append('/Users/vadim.turko/Documents/GitHub/sommed/lab3')
+from huffman import huffmandict, huffmanenco
+from entropy import entropy
 
 
 img_name = 'test_images/kodim14.png'
 img = cv.cvtColor(cv.imread(img_name)
                   [..., ::-1][:256, :256], cv.COLOR_BGR2GRAY)
 
-p = count_p(img)
-hist = histogram(img)
+ref = np.arange(256)
 
+variances = {}
+huff_dict = {}
+values, counts = np.unique(img, return_counts=True)
+for index, value in np.ndenumerate(values):
+    huff_dict[value] = counts[index]
+    variances[value] = counts[index] / img.size
 
-def entropy(img):
-    ent = 0
-    for _, val in np.ndenumerate(img):
-        ent += p[val] * log2(p[val])
-    return -ent
+hist = np.zeros(ref.size)
+for k, v in variances.items():
+    hist[k] = v
 
+#plt.subplot(1, 2, 1)
+#plt.bar(ref,  hist)
+#plt.subplot(1, 2, 2)
+#plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+#plt.show()
 
-plt.subplot(1, 2, 1)
-plt.bar(ref,  hist)
-plt.subplot(1, 2, 2)
-plt.imshow(img, cmap='gray', vmin=0, vmax=255)
-plt.show()
-
-ent = entropy(img)
+ent = entropy(img, variances)
+huff_dict = huffmandict(huff_dict)
+encoded_img = huffmanenco(img, huff_dict)
 print(ent)
