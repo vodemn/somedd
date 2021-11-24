827b341bf2d9f1a1cc7f0c363aee8e30d70f2ca6
Motans <romamakovskii@gmail.com>
2021-11-24 22:16:29 +0300

    refactor

--- lab5/zig_zag.py
+++ lab5/zig_zag.py
@@ -2,7 +2,9 @@ import numpy as np
 
 def zig_zag(img: np.ndarray) -> np.ndarray:
     img = np.flip(img, axis=0)
-    diags = [img.diagonal(i) if i % 2 == 0 
-                else np.flip(img.diagonal(i))
-                    for i in range(-img.shape[0] + 1, img.shape[1])]
-    return np.concatenate(diags)
+    diagonals = [img.diagonal(i)
+                for i in range(-img.shape[0] + 1, img.shape[1])]
+    diagonals = [diag if i % 2 == 0 
+                    else np.flip(diag)
+                        for i, diag in enumerate(diagonals)]
+    return np.concatenate(diagonals)
