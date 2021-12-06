ee7a4b25b0b6c34675f740ac47e28de6f0d8b835
Vadim <vadim.turko@gmail.com>
2021-12-06 16:37:57 +0300

    lab7

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -16,23 +16,25 @@ def get_dataset(set_name):
 
 inputs, targets = get_dataset('training')
 targets_count = 10
-w = np.random.uniform(-np.sqrt(6/(10+256)), np.sqrt(6/(10+256)), (10, 256))
+inputs_ext = np.vstack([inputs, np.ones((1, 1000))])
+w = np.random.normal(size=(10, 257)) / 257
 epoch_errors = []
-for i in range(100):
-    h = sigmoid(np.matmul(w, inputs) + np.zeros(targets_count)[:, None])
+for i in range(1000):
+    h = sigmoid(np.matmul(w, inputs_ext))
     errors = (h - targets)**2
     epoch_errors.append(np.mean(errors))
-    derive_error = np.matmul((2 * (h-targets) * h * (1 - h) / 1000), inputs.T)
+    derive_error = np.matmul((2 * (h-targets) * h * (1 - h) / 1000), inputs_ext.T)
     w -= derive_error * 0.2
 
 
-plt.plot(epoch_errors)
-plt.show()
+#plt.plot(epoch_errors)
+#plt.show()
 
 
 test, targets = get_dataset('test')
-result = sigmoid(np.matmul(w, test) + np.zeros(targets_count)[:, None])
+test_ext = np.vstack([test, np.ones((1, 9000))])
+result = sigmoid(np.matmul(w, test_ext))
 
 digits = np.argmax(targets, axis=0)
 pred_digits = np.argmax(result, axis=0)
-print(np.sum(digits == pred_digits))
+print(np.sum(digits == pred_digits) / 9000)
